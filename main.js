68ab885184936d71900395b2f90dd0b1a60c9268
Zeekkers <jesselehtinen197@gmail.com>
2025-11-16 19:01:54 +0200

    Lisätty main-haaran rivi ennen rebasea

--- main.js
+++ main.js
@@ -2,3 +2,4 @@ print("Hello Git project!")
 console.log("Tässä uusi toiminto  main.js");
 console.log("Feature logging from feature-main-logging branch");
 console.log("Cherry-pickattu ominaisuus feature-cherry-haarasta");
+console.log("Main-haaran muutos ennen rebasea");
