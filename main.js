7c09a591c060bc69b6eab097683bd6d81ec74bd3
Zeekkers <jesselehtinen197@gmail.com>
2025-11-16 17:39:03 +0200

    Lisätty cherry-pick testirivi main.js-tiedostoon

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
 print("Hello Git project!")
 console.log("Tässä uusi toiminto  main.js");
 console.log("Feature logging from feature-main-logging branch");
+console.log("Cherry-pickattu ominaisuus feature-cherry-haarasta");
