aca5e90602c7975a6abd1a1536e17a73930be8e5
Zeekkers <jesselehtinen197@gmail.com>
2025-11-16 17:34:30 +0200

    Revertoitu huono commit main.js-tiedostosta

--- main.js
+++ main.js
@@ -1,5 +1,3 @@
 print("Hello Git project!")
 console.log("Tässä uusi toiminto  main.js");
 console.log("Feature logging from feature-main-logging branch");
-console.log("Väliaikainen vaihe stash testille");
-console.log("Huono commit - älä pidä tätä");
